953e4a6644d612eb1145cd68cfa90750fbd53ec7
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-10 03:39:27 +0530

    error log implemented

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = new easyHTTP;
 
 // Get posts
-http.get('https://jsonplaceholder.typicode.com/posts',  function(response) {
-  console.log(response);
+http.get('https://jsonplaceholder.typicode.com/posts1',  function(err, posts) {
+  if(err){
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
 });
--- easyhttp.js
+++ easyhttp.js
@@ -11,7 +11,9 @@ easyHTTP.prototype.get = function(url, callback) {
   let self = this;
   this.http.onload = function() {
       if(self.http.status === 200) {
-        callback(self.http.responseText);
+        callback(null, self.http.responseText);
+      } else {
+        callback('Error: ' + self.http.status);
       }
   }
 
