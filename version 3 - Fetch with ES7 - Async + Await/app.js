8bd14819deeaceaf99d05b387543bfe2162bc371
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-12 05:32:28 +0530

    DELETE --async / await

--- version 3 - Fetch with ES7 - Async + Await/app.js
+++ version 3 - Fetch with ES7 - Async + Await/app.js
@@ -7,24 +7,24 @@ const http = new EasyHTTP;
 
 // Add Users
 
-// User Data
-const data = {
-  name: 'John Doe',
-  username: 'johndoe',
-  email: 'jdoe@gmail.com'
-}
+// // User Data
+// const data = {
+//   name: 'John Doe',
+//   username: 'johndoe',
+//   email: 'jdoe@gmail.com'
+// }
 
-// // Create Post 
-// http.post('https://jsonplaceholder.typicode.com/users', data)
+// // // Create Post 
+// // http.post('https://jsonplaceholder.typicode.com/users', data)
+// // .then(data => console.log(data))
+// // .catch(err => console.log(err));
+
+// // Update Post
+// http.put('https://jsonplaceholder.typicode.com/users/1', data)
 // .then(data => console.log(data))
 // .catch(err => console.log(err));
 
-// Update Post
-http.put('https://jsonplaceholder.typicode.com/users/1', data)
-.then(data => console.log(data))
-.catch(err => console.log(err));
-
-// // Delete Users
-// http.delete('https://jsonplaceholder.typicode.com/users/10')
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
+// Delete Users
+http.delete('https://jsonplaceholder.typicode.com/users/10')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
--- version 3 - Fetch with ES7 - Async + Await/easyhttp-3.js
+++ version 3 - Fetch with ES7 - Async + Await/easyhttp-3.js
@@ -47,17 +47,15 @@ class EasyHTTP {
   }
 
   // Make an HTTP Delete Request
-  delete(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
+  async delete(url) {
+    const response = await fetch(url, {
         method: 'DELETE',
         headers: {
           'content-type': 'application/json'
         }
-      })
-    .then(res => res.json())
-    .then(() => resolve('Resource Deleted...'))
-    .catch(err => reject(err));
-    });
+      });
+
+      const resData = await 'Resource Deleted...';
+      return resData;
   }
  }
